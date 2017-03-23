ddcaa4b7e963babb6c7d82439b215a0cf98ac62f
siyudong <siyudong7170@gmail.com>
2017-03-23 11:13:22 -0400

    assignment for Mar.23
    I ran into errors and I cannot fix them...
    
    (scraper) Siyus-MacBook-Air:crime siyudong$ python scrape.py
    /Users/siyudong/.virtualenvs/scraper/lib/python2.7/site-packages/requests/packages/urllib3/util/ssl_.py:334: SNIMissingWarning: An HTTPS request has been made, but the SNI (Subject Name Indication) extension to TLS is not available on this platform. This may cause the server to present an incorrect TLS certificate, which can cause validation failures. You can upgrade to a newer version of Python to solve this. For more information, see https://urllib3.readthedocs.io/en/latest/advanced-usage.html#ssl-warnings
      SNIMissingWarning
    /Users/siyudong/.virtualenvs/scraper/lib/python2.7/site-packages/requests/packages/urllib3/util/ssl_.py:132: InsecurePlatformWarning: A true SSLContext object is not available. This prevents urllib3 from configuring SSL appropriately and may cause certain SSL connections to fail. You can upgrade to a newer version of Python to solve this. For more informa

---
+++ scrape.py
@@ -0,0 +1,27 @@
+import csv
+import requests
+from BeautifulSoup import BeautifulSoup
+
+url = 'https://pressgallery.house.gov/member-data/demographics/women'
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html)
+table = soup.find('table')
+
+list_of_rows = []
+for row in table.findAll('tr')[1:-1]:
+    list_of_cells = []
+    for cell in row.findAll('td'):
+    	if cell.text[0] == '(':
+    		party, state = cell.text.split(',')
+    		list_of_cells.append(party)
+    		list_of_cells.append(state)
+        else:
+	        list_of_cells.append(cell.text)
+    list_of_rows.append(list_of_cells)
+
+outfile = open("women.csv", "wb")
+writer = csv.writer(outfile)
+writer.writerow(["seniority", "member", "party_state", "start_date"])
+writer.writerows(list_of_rows)
